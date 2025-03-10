b20e06a9adc3eee2a21b38df4942e20e07f99117
Alkaiir <ivv.ven0@gmail.com>
2025-03-10 16:08:02 +0700

    Init

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=bidghjpr_m1',
+    'username' => 'rbidghjpr',
+    'password' => 'bidghjpr',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
